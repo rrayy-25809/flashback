b15d60112155f7e95c1dcd8b6acd2cb2cf1d37cf
김민결 <139607260+tatatommy6@users.noreply.github.com>
2024-07-14 20:49:46 +0900

    어느정도완성~

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, request, redirect
 from base64 import b64encode
 from PIL import Image
 import io
+import app as apppppp
+import img_generating_clear_canvas
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'bmp', 'tiff', 'svg', 'webp', 'ico'])   #변환 가능한 확장자명들
 app = Flask(__name__) #서버 선언
 
-def convert_and_send(resized_img,prompt, seed, position):
+def convert_and_send(resized_img, prompt, seed, position):
     # 리사이즈된 이미지를 다시 파일 객체로 변환
     img_byte_arr = io.BytesIO()
     resized_img.save(img_byte_arr, format='JPEG')
@@ -24,7 +26,7 @@ def allowed_file(filename:str):
     if '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS:
         return  True
     return False
- 
+
 @app.route("/")
 def main():
     return render_template("index.html")#메인페이지
@@ -42,20 +44,20 @@ def post_image():
         prompt = generate_prompt()"""
     if f and allowed_file(f.filename):  # 파일이 존재하고 변환 가능한 확장자 명을 가졌다면
         img = Image.open(io.BytesIO(f.read()))  # 이미지 파일을 읽고 Pillow로 열기
-        #img.save("static/original_image.jpg","JPEG")
-        width, height = img.size  # 이미지의 해상도 가져오기
-        print(f"Original resolution: {width}x{height}")  # 원본 이미지 해상도를 출력
+        img.save("static/original_image.png","PNG")
+        img_generating_clear_canvas.canvas_clear() #이미지에 투명 캔버스 씌우는 코드
+        print("wddf")
+        apppppp.image_processing()  #변환된 이미지를 outpainting하는 코드
+        # if width>=height:
+        #     new_width = 1024
+        #     new_height = int((float(height)/float(width))*1024)
+        # else:
+        #     new_height = 1024
+        #     new_width = int((float(width)/float(height))*1024)
 
-        if width>=height:
-            new_width = 1024
-            new_height = int((float(height)/float(width))*1024)
-        else:
-            new_height = 1024
-            new_width = int((float(width)/float(height))*1024)
-
-        # 이미지 크기 조정
-        resized_img = img.resize((new_width, new_height),Image.LANCZOS)
-        print(f"Resized resolution: {new_width}x{new_height}")  # 변경된 이미지 해상도 출력
+        # # 이미지 크기 조정
+        # resized_img = img.resize((new_width, new_height),Image.LANCZOS)
+        # print(f"Resized resolution: {new_width}x{new_height}")  # 변경된 이미지 해상도 출력
 
         #TODO: 새로운 API 이용해서 resized_img와 prompt 변수 입력받아서 아웃페인팅
 
