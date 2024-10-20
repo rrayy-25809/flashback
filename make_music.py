7e3cea4ebf63bae7790d8c2bf49371dd84f09918
김민결 <139607260+tatatommy6@users.noreply.github.com>
2024-10-20 13:15:52 +0900

    한개만 출력하기
    테스트 현재 불가(서버에러)

--- make_music.py
+++ make_music.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+import json
 load_dotenv(verbose=True)
 SUNO_AI_API_KEY=os.getenv('SUNO_AI_API_KEY')
 
@@ -10,9 +11,20 @@ headers = {
     "Content-Type": "application/json"
 }
 payload = {
-    "prompt": "create a music that ",
+    "prompt": "create a healing music ",
     "make_instrumental": True,
     "wait_audio": True
 }
 response = requests.post(url, json=payload, headers=headers)
-print(response.content)
+
+if 200<= response.status_code <=299:
+    json_data = json.loads(response.content)
+    for item in json_data:
+        item.get("audio_url"+",")
+    output=item.get("audio_url")
+    output.split(",")
+    print(output[1])
+else:
+    print(f"Failed with status code: {response.status_code}")
+    print(response.text)
+    
