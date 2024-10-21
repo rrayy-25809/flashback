207c019e0d521451ba2946cd35b4b01e038da422
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2024-10-21 23:25:53 +0900

    Changes to be committed: 영상 뷰어 시점 변환 	modified:   img_to_mp4.py 	modified:   templates/viewer.html 	modified:   templates/viewer_video.html

--- img_to_mp4.py
+++ img_to_mp4.py
@@ -10,19 +10,19 @@ dotenv.load_dotenv()
 client = RunwayML(api_key=os.getenv('RUNWAY_API_SECRET'))
 
 def video_generate(file:str, story:str):
-  # 이미지에서 비디오 생성 작업
-  task = client.image_to_video.create(
+    # 이미지에서 비디오 생성 작업
+    task = client.image_to_video.create(
     model='gen3a_turbo',
     prompt_image=f'https://playflashback.xyz/static/{file}.png',
     prompt_text=story,
-  )
-  task_id = task.id
+    )
+    task_id = task.id
 
-  time.sleep(10)  #폴링 하기 전에 1초 기다리기
-  task = client.tasks.retrieve(task_id)
-  while task.status not in ['SUCCEEDED', 'FAILED']:
     time.sleep(10)  #폴링 하기 전에 1초 기다리기
     task = client.tasks.retrieve(task_id)
+    while task.status not in ['SUCCEEDED', 'FAILED']:
+        time.sleep(10)  #폴링 하기 전에 1초 기다리기
+        task = client.tasks.retrieve(task_id)
 
-  print('Task complete:', task)
-  return task.output
+    print('Task complete:', task)
+    return task.output
